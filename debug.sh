84f826a8d7adac588f2bffeb7412609685694d3d
Marcel Straub <marcel@straub-nv.de>
2018-05-21 13:26:02 +0200

    Some debugging stuff

---
+++ debug.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# This cronjob generates a text file dump with the current channel usage which is used by the munin plugin.
+# This reduces the munin runtime.
+
+# source the configuration
+source /etc/default/munin-docsis
+
+echoerr() { echo "$@" 1>&2; }
+
+# configure stick
+# disable sleep mode
+/usr/local/bin/disableDVBSleep.sh
+
+# set stick to DVB-C mode
+/usr/bin/dvb-fe-tool -d DVBC/ANNEX_A &> /dev/null
+
+
+sum=0
+for freq in $freqs
+do
+	# Tune to frequency
+	${DVBTUNE} -f $freq -s 6952 -qam 256 &> /dev/null
+	# Get bandwidth usage
+	bandwidth=`timeout 3 ${DVBSNOOP} -adapter 0 -s bandwidth 8190 -n 5000 -hideproginfo | awk -F: 'END { print $NF }' | sed 's/^[ t]*//' | awk '{printf "%i", $1 * 1000}'`
+	echo $freq.value $bandwidth 
+	sum=$( expr $sum + $bandwidth )
+done
+echo sum.value $sum 
